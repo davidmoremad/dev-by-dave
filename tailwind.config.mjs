7a57ececc5b28293649c1e608c3ecc2edf7e8c0f
David Amrani <davidmorenomad@gmail.com>
2024-10-29 23:33:12 +0100

    Minimal changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,8 +2,25 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#4400ff',
+        secondary: '#682fff',
+        dark: {
+          background: '#121212',
+          surface: '#282828',
+          text: '#F9F9F9',
+          subtext: '#A0A0A0'
+        },
+        light: {
+          background: '#F9F9F9',
+          surface: '#E5E5E5',
+          text: '#121212',
+          subtext: '#A0A0A0',
+        },
+      },
+    },
   },
   plugins: [],
-  darkMode: 'class',
+  darkMode: 'selector',
 }
